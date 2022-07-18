1a65c423bc846d53a9431f3af4fd4937a75c8834
xxseva44 <dev.xxseva44@gmail.com>
2022-07-18 01:28:04 -0400

    Add config TARGET_USES_CUSTOM_MANIFEST to bypass the common manifest

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,9 +145,15 @@ LOC_HIDL_VERSION := 3.0
 USE_DEVICE_SPECIFIC_GPS := true
 
 # HIDL
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 TARGET_FS_CONFIG_GEN += $(COMMON_PATH)/config.fs
+
+# HIDL Manifest
+TARGET_USES_CUSTOM_MANIFEST := false
+
+ifneq ($(TARGET_USES_CUSTOM_MANIFEST), false)
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+endif
 
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
